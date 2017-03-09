98cfc9f54013a1ebab8883316cddd49d7b9fa495
Dereje Deressa <derejetadese@gmail.com>
2017-03-08 23:29:55 -0500

    assignment 9 dom scripting events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,18 +1,16 @@
  var pointsArray = document.getElementsByClassName('point');
- 
- var animatePoints = function(points) {
-
-     
-     var pointAnimate = function(number) {
-         points[number].style.opacity = 1;
-         points[number].style.transform = "scaleX(1) translateY(0)";
-         points[number].style.msTransform = "scaleX(1) translateY(0)";
-         points[number].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var pointAnimate = function(points) {
+         points.style.opacity = 1;
+         points.style.transform = "scaleX(1) translateY(0)";
+         points.style.msTransform = "scaleX(1) translateY(0)";
+         points.style.WebkitTransform = "scaleX(1) translateY(0)";
          
      };
-     for (var i = 0; i < points.length; i++){
-     pointAnimate(i);
-    };
+
+ var animatePoints = function(points) {
+
+     forEach(points, pointAnimate);
+
  };
  window.onload = function() {
       if (window.innerHeight > 950) {
